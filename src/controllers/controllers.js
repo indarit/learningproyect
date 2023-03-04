c67cd5d3da87ca8774f3e7f0644755a5d13f6c32
indarit <109621400+indarit@users.noreply.github.com>
2023-03-04 15:10:44 +0100

    listo endpoint grupos,alumnos, modulos y semanas

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -2,37 +2,60 @@ const pool = require("../db");
 const bcrypt = require("bcrypt");
 const jwtGenerator = require("../utils/jwtGenerator");
 
-// Sentences SQL
-const _getAll = "SELECT * FROM alumno";
-const get_sem =
-  "select nombre_modulo, nombre_curso, numero_semana " +
-  "from modulo " +
-  "join curso c on modulo.curso_id = c.id " +
-  "join semana s on modulo.id = s.modulo_id; ";
+// SQL
+const get_Alunos = "SELECT * FROM alumno";
+const get_Modulos = "select * from modulo";
+const get_Grupos = "select * from grupo";
+const get_semanas = "select  nombre_modulo, total_semanas from modulo m";
 const inserAlumno =
   "INSERT INTO alumno (nombre_alumno, apellido_alumno, nro_identif, dir_email, dir_github, telefono, nacionalidad, fecha_nac, grupo_id, estado, password ) VALUES ($1 ,$2 ,$3 , $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *";
+
 const getAll = async (req, res) => {
   try {
-    await pool.query(get_sem, (error, result) => {
+    await pool.query(get_Alunos, (error, result) => {
       res.json(result.rows);
     });
   } catch (error) {
     res.json({ error: error.message });
   }
 };
-const createAlumno = async (req, res) => {
+
+const getModulos = async (req, res) => {
+  try {
+    await pool.query(get_Modulos, (error, result) => {
+      res.json(result.rows);
+    });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+const getGrupos = async (req, res) => {
+  try {
+    await pool.query(get_Grupos, (error, result) => {
+      res.json(result.rows);
+    });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+const getSemanas = async (req, res) => {
+  try {
+    await pool.query(get_semanas, (error, result) => {
+      res.json(result.rows);
+    });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+
+
+
+async function createAlumno(req, res) {
   const {
-    nombre,
-    apellido,
-    identificacion,
-    email,
-    github,
-    telefono,
-    nacionalidad,
-    fechanac,
-    grupo,
-    estado,
-    password,
+    nombre, apellido, identificacion, email, github, telefono, nacionalidad, fechanac, grupo, estado, password,
   } = req.body;
   console.log(req.body);
   try {
@@ -59,9 +82,12 @@ const createAlumno = async (req, res) => {
     console.error(error.message);
     res.status(400).json(`this  already exist!`);
   }
-};
+}
 
 module.exports = {
   getAll,
   createAlumno,
+  getModulos,
+  getGrupos,
+  getSemanas,
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,17 @@
 const { Router } = require("express");
-const { getAll, createAlumno } = require("../controllers/controllers");
+const {
+  getAll,
+  getModulos,
+  getGrupos,
+  getSemanas,
+  createAlumno,
+} = require("../controllers/controllers");
 
 const router = Router();
 
 router.get("/alumnos", getAll);
+router.get("/modulos", getModulos);
+router.get("/grupos", getGrupos);
+router.get("/semanas", getSemanas);
 router.post("/registro", createAlumno);
 module.exports = router;
